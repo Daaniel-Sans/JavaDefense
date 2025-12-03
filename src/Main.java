7207f5a2df4fff8461846ddc3448088d057ee6ce
Daaniel-Sans <danpavtel@gmail.com>
2025-12-03 11:29:20 +0100

    Finished exercises and verify main class

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,54 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import pokemon.Pokemon;
+import pokemon.Pocion;
+import personas.Persona;
+import personas.Entrenador;
+import personas.LiderGimnasio;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //Crear 4 Pokémon e imprimir sus toString
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Pokemon pikachu = new Pokemon ("Pikachu", 3, 78, 66);
+        Pokemon geodude = new Pokemon ("Geodude", 4, 366, 43);
+        Pokemon psyduck = new Pokemon ("Psyduck", 65, 179, 67);
+        Pokemon snorlax = new Pokemon ("Snorlax", 6, 100, 100);
+
+        System.out.println(pikachu);
+        System.out.println(geodude);
+        System.out.println(psyduck);
+        System.out.println(snorlax);
+
+        System.out.println("---------------");
+        //Ejecutar 2 ataques e imprimir los puntos de salud restantes de los Pokémon atacados
+        pikachu.atacar(geodude); // pikachu ataca a geodude
+        psyduck.atacar(snorlax); // psyduck ataca snorlax
+
+        System.out.println("Puntos de salud restantes:");
+        System.out.println(geodude.getNombre() + ": " + geodude.getPuntosSalud());
+        System.out.println(snorlax.getNombre() + ": " + snorlax.getPuntosSalud());
+
+        System.out.println("-------------");
+        //Crear 1 poción, curar a un Pokémon atacado e imprimir los puntos de salud restantes
+        Pocion pocion = new Pocion(10, 50.0);
+        pocion.curar(snorlax);
+
+        System.out.println("Después de usar la poción:");
+        System.out.println(snorlax.getNombre() + ": " + snorlax.getPuntosSalud());
+        System.out.println("-----------");
+
+        //Crear 2 entrenadores e imprimir sus toString
+
+        Entrenador entrenador1 = new Entrenador ("Ash", 16, pikachu, 6, Entrenador.Clase.MONTANYERO);
+        Entrenador entrenador2 = new Entrenador ("Gary", 18, geodude, 5, 6, Entrenador.Clase.DOMINGUERO);
+
+        System.out.println(entrenador1);
+        System.out.println(entrenador2);
+
+        //Crear 1 líder de gimnasio e imprimir su toString
+
+        LiderGimnasio Manue = new LiderGimnasio("Manué", 18, pikachu, 6, "El polígono San Pablo", "Sevilla", true);
+        System.out.println(Manue);
+
     }
 }
